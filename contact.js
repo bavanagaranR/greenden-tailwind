02f20865da3aa236cef7e9e201ece91291df6b0a
bavanagaranR <bavanab42@gmail.com>
2024-08-12 23:57:09 +0530

    seach enable and sidebar

---
+++ contact.js
@@ -0,0 +1,15 @@
+//selcting side nav,menu
+
+
+var sidenav=document.getElementById("sidenav")
+var menuicon=document.getElementById("menuicon")
+var closenav=document.getElementById("closenav")
+menuicon.addEventListener("click",function()
+{
+    sidenav.style.right=0
+})
+
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
